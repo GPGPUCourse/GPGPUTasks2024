cab7cec7df56f62f7e9371ab6aaee3c0e71fd3b9
Sasha <amosin2002@mail.ru>
2024-09-22 00:47:55 +0300

    Path is fixed

--- src/main.cpp
+++ src/main.cpp
@@ -96,8 +96,7 @@ int main() {
     // напечатав исходники в консоль (if проверяет, что удалось считать хоть что-то)
     std::string kernel_sources;
     {
-        std::ifstream file(
-                "/home/vlad/Sasha/4_Course/courses/computing_on_video_cards/GPGPUTasks2024/src/cl/aplusb.cl");
+        std::ifstream file("src/cl/aplusb.cl");
         kernel_sources = std::string(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>());
         if (kernel_sources.size() == 0) {
             throw std::runtime_error("Empty source file! May be you forgot to configure working directory properly?");
