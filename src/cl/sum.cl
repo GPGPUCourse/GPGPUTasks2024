b675f7fcb882ead088943754c9a7321da218e8ee
Daniil Ushkov <udav@mts.ru>
2024-09-23 21:02:31 +0300

    sum_1

--- src/cl/sum.cl
+++ src/cl/sum.cl
@@ -1,1 +1,10 @@
-// TODO
+__kernel void sum_1(__global int *arr, unsigned int n, __global int *sum)
+{
+    int idx = get_global_id(0);
+
+    if (idx > n) {
+        return;
+    }
+
+    atomic_add(sum, arr[idx]);
+}
--- src/main_sum.cpp
+++ src/main_sum.cpp
@@ -1,7 +1,10 @@
 #include <libutils/misc.h>
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
+#include <libgpu/context.h>
+#include <libgpu/shared_device_buffer.h>
 
+#include "cl/sum_cl.h"
 
 template<typename T>
 void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line)
@@ -14,6 +17,37 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
+void execKernel(const char *name, std::vector<unsigned int> &as, unsigned int n, unsigned int reference_sum, int benchmarkingIters)
+{
+    std::cout << "Kernel " << name << std::endl;
+
+    gpu::gpu_mem_32u as_gpu;
+    as_gpu.resizeN(n);
+    as_gpu.writeN(as.data(), n);
+
+    unsigned int sum = 0;
+    gpu::gpu_mem_32u sum_gpu;
+    sum_gpu.resizeN(1);
+    sum_gpu.writeN(&sum, 1);
+
+    ocl::Kernel kernel(sum_kernel, sum_kernel_length, name);
+    kernel.compile();
+
+    unsigned int workGroupSize = 32;
+    unsigned int workSpaceSize = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
+
+    timer t;
+    for (int iter = 0; iter < benchmarkingIters; ++iter) {
+        kernel.exec(gpu::WorkSize(workGroupSize, workSpaceSize), as_gpu, n, sum_gpu);
+        t.nextLap();
+    }
+
+    sum_gpu.readN(&sum, 1);
+    EXPECT_THE_SAME(reference_sum, sum, "GPU result should be consistent!");
+
+    std::cout << "GPU OMP: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+    std::cout << "GPU OMP: " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+}
 
 int main(int argc, char **argv)
 {
@@ -58,7 +92,11 @@ int main(int argc, char **argv)
     }
 
     {
-        // TODO: implement on OpenCL
-        // gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        gpu::Context context;
+        context.init(device.device_id_opencl);
+        context.activate();
+
+        execKernel("sum_1", as, n, reference_sum, benchmarkingIters);
     }
 }
