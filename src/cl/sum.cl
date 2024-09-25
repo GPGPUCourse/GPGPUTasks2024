604834115a0027428c947d458a7270501eb40149
elteammate <elteammate@gmail.com>
2024-09-25 21:45:15 +0300

    Done

--- src/cl/sum.cl
+++ src/cl/sum.cl
@@ -1,1 +1,127 @@
-// TODO
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.cl>
+#endif
+
+#line 6
+
+__kernel void sum01_global_atomic(
+    __global unsigned int *a,
+    unsigned int n,
+    __global unsigned int *result
+) {
+    int i = get_global_id(0);
+    if (i < n) {
+        atomic_add(result, a[i]);
+    }
+}
+
+#define VALUES_PER_WORKITEM 64
+
+__kernel void sum02_multiple_per_warp_non_coalesced(
+        __global unsigned int *a,
+        unsigned int n,
+        __global unsigned int *result
+) {
+    int i = get_global_id(0);
+
+    unsigned int sum = 0;
+    for (int j = 0; j < VALUES_PER_WORKITEM; ++j) {
+        int idx = i * VALUES_PER_WORKITEM + j;
+        if (idx < n) {
+            sum += a[idx];
+        }
+    }
+
+    atomic_add(result, sum);
+}
+
+__kernel void sum03_multiple_per_warp_coalesced(
+        __global unsigned int *a,
+        unsigned int n,
+        __global unsigned int *result
+) {
+    int i = get_global_id(0);
+
+    unsigned int sum = 0;
+    for (int j = 0; j < n; j += get_global_size(0)) {
+        int idx = i + j;
+        if (idx < n) {
+            sum += a[idx];
+        }
+    }
+
+    atomic_add(result, sum);
+}
+
+#define WORKGROUP_SIZE 128
+
+__kernel void sum04_local_memory_tree(
+        __global unsigned int *a,
+        unsigned int n,
+        __global unsigned int *result
+) {
+    int gid = get_global_id(0);
+    int lid = get_local_id(0);
+    int two_lid = lid << 1;
+    int two_gid = gid << 1;
+
+    __local unsigned int buf[WORKGROUP_SIZE * 2];
+
+    int idx = get_group_id(0) * (WORKGROUP_SIZE * 2) + lid;
+    if (idx < n) {
+        buf[lid] = a[idx];
+    } else {
+        buf[lid] = 0;
+    }
+
+    idx += WORKGROUP_SIZE;
+    if (idx < n) {
+        buf[lid + WORKGROUP_SIZE] = a[idx];
+    } else {
+        buf[lid + WORKGROUP_SIZE] = 0;
+    }
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+    for (int i = WORKGROUP_SIZE; i > 0; i >>= 1) {
+        if (lid < i) {
+            buf[lid] += buf[lid + i];
+        }
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+
+    if (lid == 0) {
+        atomic_add(result, buf[0]);
+    }
+}
+
+__kernel void sum05_global_tree(
+        __global unsigned int *a,
+        __global unsigned int *b,
+        unsigned int n,
+        unsigned int half_n
+) {
+    int i = get_global_id(0);
+    unsigned int sum = a[i];
+    int j = i + half_n;
+    if (j < n) {
+        sum += a[j];
+    }
+    b[i] = sum;
+}
+
+__kernel void sum06_wider_global_tree(
+        __global unsigned int *a,
+        __global unsigned int *b,
+        unsigned int n,
+        unsigned int k
+) {
+    int idx = get_global_id(0);
+    unsigned int sum = 0;
+    for (int i = 0; i < VALUES_PER_WORKITEM; ++i) {
+        if (idx < n) {
+            sum += a[idx];
+        }
+        idx += k;
+    }
+    b[get_global_id(0)] = sum;
+}
--- src/main_sum.cpp
+++ src/main_sum.cpp
@@ -1,7 +1,12 @@
 #include <libutils/misc.h>
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
+#include <functional>
 
+#include "cl/sum_cl.h"
+
+#include "libgpu/context.h"
+#include "libgpu/shared_device_buffer.h"
 
 template<typename T>
 void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line)
@@ -57,8 +62,114 @@ int main(int argc, char **argv)
         std::cout << "CPU OMP: " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
     }
 
-    {
-        // TODO: implement on OpenCL
-        // gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+    gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+    gpu::Context context;
+    context.init(device.device_id_opencl);
+    context.activate();
+
+    const unsigned int ZERO = 0;
+    const unsigned int workGroupSize = 128;
+    const unsigned int values_per_workitem = 64;
+
+    std::cout << "GPU: " << std::endl;
+
+    gpu::gpu_mem_32u as_gpu, tmp1_gpu, tmp2_gpu;
+    gpu::gpu_mem_32u result_gpu;
+    as_gpu.resizeN(n);
+    tmp1_gpu.resizeN(n);
+    tmp2_gpu.resizeN(n);
+    result_gpu.resizeN(1);
+    as_gpu.writeN(as.data(), n);
+
+    std::vector<std::pair<const char *, std::function<void(ocl::Kernel &kernel)>>> kernels = {
+            {"sum01_global_atomic", [&](ocl::Kernel &kernel) {
+                kernel.exec(gpu::WorkSize(workGroupSize, n), as_gpu, n, result_gpu);
+            }},
+            {"sum02_multiple_per_warp_non_coalesced", [&](ocl::Kernel &kernel) {
+                kernel.exec(gpu::WorkSize(workGroupSize, (n + values_per_workitem - 1) / values_per_workitem), as_gpu, n, result_gpu);
+            }},
+            {"sum03_multiple_per_warp_coalesced", [&](ocl::Kernel &kernel) {
+                const int k = 128;
+                // why????
+                // kernel.exec(gpu::WorkSize(workGroupSize, ((n + k - 1) / k + 127) / 128 * 128), as_gpu, n, result_gpu);
+                kernel.exec(gpu::WorkSize(workGroupSize, (n + k - 1) / k), as_gpu, n, result_gpu);
+            }},
+            {"sum04_local_memory_tree", [&](ocl::Kernel &kernel) {
+                kernel.exec(gpu::WorkSize(workGroupSize, (n + 1) / 2), as_gpu, n, result_gpu);
+            }},
+            {"sum05_global_tree", [&](ocl::Kernel &kernel) {
+                int size = n;
+                const int align = 128;
+                gpu::gpu_mem_32u *a = &tmp1_gpu;
+                gpu::gpu_mem_32u *b = &tmp2_gpu;
+                as_gpu.copyToN(*a, size);
+                while (size > 1024) {
+                    int half_size = ((size + 1) / 2 + align - 1) / align * align;
+                    kernel.exec(
+                            gpu::WorkSize(workGroupSize, half_size),
+                            *a, *b,
+                            size, half_size
+                    );
+                    size = half_size;
+                    std::swap(a, b);
+                }
+                unsigned int values[1024];
+                a->readN(values, size);
+                unsigned int sum = 0;
+                for (int i = 0; i < size; ++i) {
+                    sum += values[i];
+                }
+                result_gpu.writeN(&sum, 1);
+            }},
+            {"sum06_wider_global_tree", [&](ocl::Kernel &kernel) {
+                int size = n;
+                gpu::gpu_mem_32u *a = &tmp1_gpu;
+                gpu::gpu_mem_32u *b = &tmp2_gpu;
+                as_gpu.copyToN(*a, size);
+                while (size > 1024) {
+                    int part_size = (size + values_per_workitem - 1) / values_per_workitem;
+                    kernel.exec(
+                            gpu::WorkSize(workGroupSize, part_size),
+                            *a, *b,
+                            size, part_size
+                    );
+                    size = part_size;
+                    std::swap(a, b);
+                }
+                unsigned int values[1024];
+                a->readN(values, size);
+                unsigned int sum = 0;
+                for (int i = 0; i < size; ++i) {
+                    sum += values[i];
+                }
+                result_gpu.writeN(&sum, 1);
+            }}
+    };
+
+    for (const auto &kernel_info : kernels) {
+        const char *kernel_name = kernel_info.first;
+        auto exec = kernel_info.second;
+
+        ocl::Kernel kernel(sum_kernel, sum_kernel_length, kernel_name);
+        kernel.compile();
+
+        {
+            timer t;
+
+            for (int iter = 0; iter < benchmarkingIters; ++iter) {
+                result_gpu.writeN(&ZERO, 1);
+
+                exec(kernel);
+
+                unsigned int result;
+                result_gpu.readN(&result, 1);
+                EXPECT_THE_SAME(reference_sum, result, "Result should be equal to CPU result!");
+                t.nextLap();
+            }
+
+            std::cout << " - " << kernel_name << ":" << std::endl;
+            std::cout << "            time: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+            std::cout << "      throughput: " << (n / 1e9) / t.lapAvg() << " B/s" << std::endl;
+        }
     }
 }
