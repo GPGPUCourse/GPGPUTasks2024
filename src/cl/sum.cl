6313b23a62bea24f3ef410f52c20f7ac9c91c2fb
Zelourses <24527982+Zelourses@users.noreply.github.com>
2024-09-30 01:18:57 +0300

    Done third task

--- src/cl/sum.cl
+++ src/cl/sum.cl
@@ -1,1 +1,89 @@
-// TODO
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.cl>
+#endif
+
+#line 6
+
+__kernel void sum_global_atomic(__global const unsigned* arr, unsigned size, __global unsigned* result) {
+    unsigned id = get_global_id(0);
+
+    if (id < size) {
+        atomic_add(result, arr[id]);
+    }
+}
+__kernel void sum_loop(__global const unsigned* arr, unsigned size, __global unsigned* result) {
+#define VALS_PER_WORKITEM 64
+    unsigned id = get_global_id(0);
+
+    unsigned res = 0;
+    for (int i = 0; i < VALS_PER_WORKITEM; ++i) {
+        int idx = id * VALS_PER_WORKITEM + i;
+        if (idx < size) {
+            res += arr[idx];
+        }
+    }
+
+    atomic_add(result, res);
+#undef VALS_PER_WORKITEM
+}
+__kernel void sum_loop_coalesced(__global const unsigned* arr, unsigned size, __global unsigned* result) {
+#define VALS_PER_WORKITEM 64
+    const unsigned localId = get_local_id(0);
+    const unsigned groupId = get_group_id(0);
+    const unsigned localSize = get_local_size(0);
+
+    unsigned res = 0;
+    for (int i = 0; i < VALS_PER_WORKITEM; ++i) {
+        // possible overflow
+        unsigned idx = groupId * localSize * VALS_PER_WORKITEM + i * localSize + localId;
+        if (idx < size) {
+            res += arr[idx];
+        }
+    }
+    atomic_add(result, res);
+#undef VALS_PER_WORKITEM
+}
+__kernel void sum_local_mem(__global const unsigned* arr, unsigned size, __global unsigned* result) {
+#define WORKGROUP_SIZE 128
+    unsigned globalId = get_global_id(0);
+    unsigned localId = get_local_id(0);
+
+    __local unsigned localArr[WORKGROUP_SIZE];
+    localArr[localId] = globalId < size ? arr[globalId] : 0;
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    if (localId == 0) {
+        unsigned partialSum = 0;
+        for (unsigned i = 0; i < WORKGROUP_SIZE; ++i) {
+            partialSum += localArr[i];
+        }
+        atomic_add(result, partialSum);
+    }
+#undef WORKGROUP_SIZE
+}
+__kernel void sum_tree(__global const unsigned* arr, unsigned size, __global unsigned* result) {
+#define WORKGROUP_SIZE 128
+    unsigned globalId = get_global_id(0);
+    unsigned localId = get_local_id(0);
+
+    __local unsigned localArr[WORKGROUP_SIZE];
+    localArr[localId] = globalId < size ? arr[globalId] : 0;
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    for (unsigned k = WORKGROUP_SIZE; k > 1; k /= 2) {
+        if (localId * 2 < k) {
+            int a = localArr[localId];
+            int b = localArr[localId + k / 2];
+            localArr[localId] = a + b;
+        }
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+
+    if (localId == 0) {
+        atomic_add(result, localArr[0]);
+    }
+
+#undef WORKGROUP_SIZE
+}
--- src/main_sum.cpp
+++ src/main_sum.cpp
@@ -1,6 +1,10 @@
+#include "libgpu/context.h"
+#include "libgpu/shared_device_buffer.h"
+#include <libutils/fast_random.h>
 #include <libutils/misc.h>
 #include <libutils/timer.h>
-#include <libutils/fast_random.h>
+
+#include "cl/sum_cl.h"
 
 
 template<typename T>
@@ -13,6 +17,29 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 }
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
+
+// I want std::string_view..
+void runKernel(int benchmarkingIters, unsigned int reference_sum, unsigned int n, std::vector<unsigned int> &as, gpu::WorkSize size, const char* name) {
+    gpu::gpu_mem_32u gpuAS, gpuSum;
+    gpuAS.resizeN(n);
+    gpuSum.resizeN(1);
+    gpuAS.writeN(as.data(), n);
+    ocl::Kernel kernel(sum_kernel, sum_kernel_length, name);
+    kernel.compile();
+
+    timer t;
+    for (int i = 0; i < benchmarkingIters; ++i) {
+        unsigned sum = 0;
+        gpuSum.writeN(&sum, 1);
+        kernel.exec(size, gpuAS, n, gpuSum);
+        gpuSum.readN(&sum, 1);
+        t.nextLap();
+        EXPECT_THE_SAME(reference_sum, sum, "OpenCL Results must be consistent");
+    }
+
+    std::cout << "GPU kernel '" << name << "' : " << t.lapAvg() << "+-" << t.lapStd() << "s\n"
+    << "  Time: " << (n / 1000. / 1000.) / t.lapAvg() << " millions/s\n";
+}
 
 
 int main(int argc, char **argv)
@@ -28,37 +55,49 @@ int main(int argc, char **argv)
         reference_sum += as[i];
     }
 
-    {
-        timer t;
-        for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            unsigned int sum = 0;
-            for (int i = 0; i < n; ++i) {
-                sum += as[i];
-            }
-            EXPECT_THE_SAME(reference_sum, sum, "CPU result should be consistent!");
-            t.nextLap();
-        }
-        std::cout << "CPU:     " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "CPU:     " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
-    }
+//    {
+//        timer t;
+//        for (int iter = 0; iter < benchmarkingIters; ++iter) {
+//            unsigned int sum = 0;
+//            for (int i = 0; i < n; ++i) {
+//                sum += as[i];
+//            }
+//            EXPECT_THE_SAME(reference_sum, sum, "CPU result should be consistent!");
+//            t.nextLap();
+//        }
+//        std::cout << "CPU:     " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+//        std::cout << "CPU:     " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+//    }
 
-    {
-        timer t;
-        for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            unsigned int sum = 0;
-            #pragma omp parallel for reduction(+:sum)
-            for (int i = 0; i < n; ++i) {
-                sum += as[i];
-            }
-            EXPECT_THE_SAME(reference_sum, sum, "CPU OpenMP result should be consistent!");
-            t.nextLap();
-        }
-        std::cout << "CPU OMP: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "CPU OMP: " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
-    }
+//    {
+//        timer t;
+//        for (int iter = 0; iter < benchmarkingIters; ++iter) {
+//            unsigned int sum = 0;
+//            #pragma omp parallel for reduction(+:sum)
+//            for (int i = 0; i < n; ++i) {
+//                sum += as[i];
+//            }
+//            EXPECT_THE_SAME(reference_sum, sum, "CPU OpenMP result should be consistent!");
+//            t.nextLap();
+//        }
+//        std::cout << "CPU OMP: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+//        std::cout << "CPU OMP: " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+//    }
 
     {
         // TODO: implement on OpenCL
-        // gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+         gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+         gpu::Context context;
+         context.init(device.device_id_opencl);
+         context.activate();
+
+         {
+             runKernel(benchmarkingIters, reference_sum, n, as, gpu::WorkSize(128, n), "sum_global_atomic");
+             runKernel(benchmarkingIters, reference_sum, n, as, gpu::WorkSize(128, n / 64), "sum_loop");
+             runKernel(benchmarkingIters, reference_sum, n, as, gpu::WorkSize(128, n / 64), "sum_loop_coalesced");
+             runKernel(benchmarkingIters, reference_sum, n, as, gpu::WorkSize(128, n), "sum_local_mem");
+             runKernel(benchmarkingIters, reference_sum, n, as, gpu::WorkSize(128, n), "sum_tree");
+         }
     }
 }
+
