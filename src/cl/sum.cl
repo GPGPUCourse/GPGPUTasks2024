e8a7c3076b2a365f2b0424feaf0721c313834449
Rybchits <Grinya110@gmail.com>
2024-09-29 19:40:12 +0000

    implemented atomic_sum and loop_sum

--- src/cl/sum.cl
+++ src/cl/sum.cl
@@ -1,1 +1,21 @@
-// TODO
+#define VALUES_PER_WORK_ITEM 32
+#define WORKGROUP_SIZE 128
+
+__kernel void atomic_sum(__global const int *array, __global int *sum, unsigned int n) {
+    unsigned int index = get_global_id(0);
+    
+    if (index < n) {
+        atomic_add(sum, array[index]);
+    }
+}
+
+__kernel void loop_sum(__global const int *arr, __global int *sum, unsigned int n) {
+    const unsigned int index = get_global_id(0);
+    int res = 0;
+    for (int i = index * VALUES_PER_WORK_ITEM; i < (index + 1) * VALUES_PER_WORK_ITEM; ++i) {
+        if (i < n) {
+            res += arr[i];
+        }
+    }
+    atomic_add(sum, res);
+}
--- src/main_sum.cpp
+++ src/main_sum.cpp
@@ -2,6 +2,12 @@
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
 
+#include "cl/sum_cl.h"
+#include <libutils/fast_random.h>
+#include <libgpu/context.h>
+#include <libgpu/shared_device_buffer.h>
+#include <list>
+#include <string>
 
 template<typename T>
 void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line)
@@ -14,6 +20,10 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
+struct kernel_info {
+    std::string name;
+    gpu::WorkSize work_size;
+};
 
 int main(int argc, char **argv)
 {
@@ -58,7 +68,44 @@ int main(int argc, char **argv)
     }
 
     {
-        // TODO: implement on OpenCL
-        // gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        gpu::Context context;
+        context.init(device.device_id_opencl);
+        context.activate();
+
+        gpu::gpu_mem_32u buffer;
+        gpu::gpu_mem_32u result;
+
+        buffer.resizeN(n);
+        result.resizeN(1);
+
+        unsigned int work_group_size = 128;
+        unsigned int global_work_size = (n + work_group_size - 1) / work_group_size * work_group_size;
+        unsigned int n_work_groups = global_work_size / work_group_size;
+
+        buffer.writeN(as.data(), n);
+        const unsigned int init = 0;
+
+        std::list<kernel_info> kernels = {
+            {"atomic_sum", gpu::WorkSize(work_group_size, global_work_size)},
+            {"loop_sum", gpu::WorkSize(work_group_size, global_work_size / 32)},
+        };
+
+        for(const auto& info : kernels) {
+            ocl::Kernel kernel(sum_kernel, sum_kernel_length, info.name.data());
+            kernel.compile();
+
+            timer t;
+            unsigned int sum = 0;
+            for (int iter = 0; iter < benchmarkingIters; ++iter) {
+                result.writeN(&init, 1);
+                kernel.exec(info.work_size, buffer, result, n);
+                t.nextLap();
+            }
+            result.readN(&sum, 1);
+            EXPECT_THE_SAME(reference_sum, sum, "GPU result should be consistent!");
+            std::cout << "GPU " << info.name << " : " <<  t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+            std::cout << "GPU " << info.name << " : " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl << std::endl;
+        }
     }
 }
