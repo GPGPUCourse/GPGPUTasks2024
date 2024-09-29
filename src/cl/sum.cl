65e8584a2477760b5c154384193f488cf607b734
Valery Matskevich <generalretcher@gmail.com>
2024-09-29 22:00:07 +0300

    task03: sum done
    Signed-off-by: Valery Matskevich <generalretcher@gmail.com>

--- src/cl/sum.cl
+++ src/cl/sum.cl
@@ -1,1 +1,115 @@
-// TODO
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.cl>
+#endif
+
+#line 6
+
+#define WORKGROUP_SIZE 128
+#define VALUES_PER_WORKITEM 64
+
+__kernel void global_add_scan(__global const unsigned int *array,
+                              __global unsigned int *sum,
+                              unsigned int n)
+{
+    const unsigned int gid = get_global_id(0);
+
+    if (gid >= n) {
+        return;
+    }
+
+    atomic_add(sum, array[gid]);
+}
+
+__kernel void simple_cycle_scan(__global unsigned int *array, 
+                                __global unsigned int *sum, 
+                                unsigned int n)
+{
+    const unsigned int gid = get_global_id(0);
+
+    unsigned int result = 0;
+    for (int i = 0; i < VALUES_PER_WORKITEM; i++) {
+        int idx = gid * VALUES_PER_WORKITEM + i;
+
+        if (idx < n) {
+            result += array[idx];
+        }
+    }
+
+    atomic_add(sum, result);
+}
+
+__kernel void coalesced_cycle_scan(__global unsigned int *array, 
+                                   __global unsigned int *sum, 
+                                   unsigned int n)
+{
+    const unsigned int group_size = get_local_size(0);
+    const unsigned int gid = get_group_id(0);
+    const unsigned int lid = get_local_id(0);
+
+    int result = 0;
+    for (int i = 0; i < VALUES_PER_WORKITEM; i++) {
+        int idx = gid * group_size * VALUES_PER_WORKITEM + i * group_size + lid;
+
+        if (idx < n) {
+            result += array[idx];
+        }
+    }
+
+    atomic_add(sum, result);
+}
+
+__kernel void local_leader_synchronization_scan(__global const unsigned int *array,
+                                                __global unsigned int *sum,
+                                                unsigned int n)
+{
+    const unsigned int lid = get_local_id(0);
+    const unsigned int gid = get_global_id(0);
+
+    __local unsigned int buffer[WORKGROUP_SIZE];
+
+    buffer[lid] = array[gid];
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    if (lid != 0) {
+        return;
+    }
+
+    unsigned int result = 0;
+    for (int i = 0; i < WORKGROUP_SIZE; i++) {
+        result += buffer[i];
+    }
+
+    atomic_add(sum, result);
+}
+
+__kernel void divide_and_conquer_scan(__global const unsigned int *array,
+                                      __global unsigned int *sum,
+                                      unsigned int n)
+{
+    const unsigned int lid = get_local_id(0);
+    const unsigned int gid = get_global_id(0);
+    const unsigned int wid = get_group_id(0);
+
+    __local unsigned int buffer[WORKGROUP_SIZE];
+
+    if (gid < n) {
+        buffer[lid] = array[gid];
+    } else {
+        buffer[lid] = 0;
+    }
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    for (int i = WORKGROUP_SIZE; i > 1; i /= 2) {
+        if (lid * 2 < i) {
+            buffer[lid] += buffer[lid + i / 2];
+        }
+
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+
+    if (lid == 0) {
+        atomic_add(sum, buffer[0]);
+    }
+}
--- src/main_sum.cpp
+++ src/main_sum.cpp
@@ -2,6 +2,9 @@
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
 
+#include "libgpu/context.h"
+#include "libgpu/shared_device_buffer.h"
+#include "cl/sum_cl.h"
 
 template<typename T>
 void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line)
@@ -14,13 +17,42 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
+namespace {
+
+constexpr int benchmarkingIters = 10;
+constexpr unsigned int n = 100*1000*1000;
+
+void executeKernel(const char* name,
+                   const gpu::gpu_mem_32u& memory,
+                   gpu::WorkSize ws,
+                   unsigned int expected) {
+    ocl::Kernel kernel(sum_kernel, sum_kernel_length, name);
+    gpu::gpu_mem_32u result_gpu;
+    result_gpu.resizeN(1);
+
+    timer t;
+    std::string errorMessage = std::string("GPU ") + name + " result should be consistent!";
+    for (int iter = 0; iter < benchmarkingIters; ++iter) {
+        unsigned int sum = 0;
+
+        result_gpu.writeN(&sum, 1);
+        kernel.exec(ws, memory, result_gpu, n);
+        result_gpu.readN(&sum, 1);
+
+        EXPECT_THE_SAME(expected, sum, errorMessage);
+
+        t.nextLap();
+    }
+
+    std::cout << "GPU " << name << ": " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+    std::cout << "GPU " << name << ": " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+}
+
+}  // namespace
 
 int main(int argc, char **argv)
 {
-    int benchmarkingIters = 10;
-
     unsigned int reference_sum = 0;
-    unsigned int n = 100*1000*1000;
     std::vector<unsigned int> as(n, 0);
     FastRandom r(42);
     for (int i = 0; i < n; ++i) {
@@ -58,7 +90,24 @@ int main(int argc, char **argv)
     }
 
     {
-        // TODO: implement on OpenCL
-        // gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        const unsigned int workGroupSize = 128u;
+        const unsigned int valuesPerWorkItem = 64u;
+        const unsigned int workItems = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
+
+        gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        gpu::Context context;
+
+        context.init(device.device_id_opencl);
+        context.activate();
+
+        gpu::gpu_mem_32u array;
+        array.resizeN(n);
+        array.writeN(as.data(), n);
+
+        executeKernel("global_add_scan", array, gpu::WorkSize(workGroupSize, workItems), reference_sum);
+        executeKernel("simple_cycle_scan", array, gpu::WorkSize(workGroupSize, workItems / valuesPerWorkItem), reference_sum);
+        executeKernel("coalesced_cycle_scan", array, gpu::WorkSize(workGroupSize, workItems / valuesPerWorkItem), reference_sum);
+        executeKernel("local_leader_synchronization_scan", array, gpu::WorkSize(workGroupSize, workItems), reference_sum);
+        executeKernel("divide_and_conquer_scan", array, gpu::WorkSize(workGroupSize, workItems), reference_sum);
     }
 }
