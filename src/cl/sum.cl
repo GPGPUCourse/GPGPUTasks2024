1d478117877b109e4049aa62bb1b1306b8df62b3
Evgenii <eugenysvirin@gmail.com>
2024-09-29 22:50:12 +0300

    add task03

--- src/cl/sum.cl
+++ src/cl/sum.cl
@@ -1,1 +1,91 @@
 // TODO
+__kernel void atomicSum1(__global const uint *arr,
+     __global uint *sum, uint n) {
+    size_t gid = get_global_id(0);
+
+    if (n <= gid) {
+        return;
+    }
+    atomic_add(sum, arr[gid]);
+}
+
+
+#define VALUES_PER_WORKITEM 32
+__kernel void loopSum2(__global const uint *arr,
+                      __global uint *sum,
+                      uint n) {
+    const uint gid = get_global_id(0);
+
+    uint res = 0;
+    for (int i = 0; i < VALUES_PER_WORKITEM; ++i) {
+        int idx = gid * VALUES_PER_WORKITEM + i;
+        if (idx < n) {
+            res += arr[idx];
+        }
+    }
+
+    atomic_add(sum, res);
+}
+
+__kernel void loopCoalescedSum3(__global const uint *arr,
+                              __global uint *sum,
+                              uint n) {
+    size_t lid = get_local_id(0);
+    size_t wid = get_group_id(0);
+    size_t grs = get_local_size(0);
+
+    int res = 0;
+    for (int i = 0; i < VALUES_PER_WORKITEM; i++) {
+        int idx = wid * grs * VALUES_PER_WORKITEM + i * grs + lid;
+        if (idx < n)
+            res += arr[idx];
+    }
+
+    atomic_add(sum, res);
+}
+
+#define WORKGROUP_SIZE 64
+__kernel void localMemSum4(__global const uint *arr,
+                          __global uint *sum,
+                          uint n) {
+    const uint gid = get_global_id(0);
+    const uint lid = get_local_id(0);
+
+    __local uint buf[WORKGROUP_SIZE];
+
+    buf[lid] = arr[gid];
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    if (lid == 0) {
+        uint groupRes = 0;
+        for (int i = 0; i < WORKGROUP_SIZE; i++) {
+            groupRes += buf[i];
+        }
+        atomic_add(sum, groupRes);
+    }
+}
+
+__kernel void treeSum5(__global const uint *arr,
+                       __global uint *sum,
+                       uint n) {
+    const uint gid = get_global_id(0);
+    const uint lid = get_local_id(0);
+
+    __local uint buf[WORKGROUP_SIZE];
+
+    buf[lid] = gid < n ? arr[gid] : 0;
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    for (int nValues = WORKGROUP_SIZE; nValues > 1; nValues /= 2) {
+        if (2 * lid < nValues) {
+            buf[lid] += buf[lid + nValues / 2];
+        }
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+
+    if (lid == 0) {
+       atomic_add(sum, buf[0]);
+    };
+}
--- src/main_sum.cpp
+++ src/main_sum.cpp
@@ -2,6 +2,10 @@
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
 
+#include "cl/sum_cl.h"
+#include "libgpu/shared_device_buffer.h"
+#include "libgpu/context.h"
+
 
 template<typename T>
 void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line)
@@ -14,6 +18,31 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
+const unsigned int WORKGROUP_SIZE = 64;
+const unsigned int VALUES_PER_WORKITEM = 32;
+void runBenchmark(gpu::gpu_mem_32u& arr,
+                gpu::gpu_mem_32u& gpuSum,
+                const std::string& kernelName,
+                const int benchmarkingIters,
+                const uint reference_sum,
+                const uint n) {
+    ocl::Kernel kernel(sum_kernel, sum_kernel_length, kernelName);
+    timer t;
+    for (int iter = 0; iter < benchmarkingIters; ++iter) {
+        cl_uint sum = 0;
+        gpuSum.writeN(&sum, 1);
+        kernel.exec(gpu::WorkSize(WORKGROUP_SIZE, n),
+                    arr.clmem(),
+                    gpuSum.clmem(),
+                    n);
+        gpuSum.readN(&sum, 1);
+        EXPECT_THE_SAME(reference_sum, sum, "GPU result should be consistent!");
+        t.nextLap();
+    }
+    t.stop();
+    std::cout << "GPU " + kernelName + ": " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+    std::cout << "GPU " + kernelName + ": " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+}
 
 int main(int argc, char **argv)
 {
@@ -59,6 +88,22 @@ int main(int argc, char **argv)
 
     {
         // TODO: implement on OpenCL
-        // gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        gpu::Context context;
+        context.init(device.device_id_opencl);
+        context.activate();
+
+        gpu::gpu_mem_32u arr;
+        gpu::gpu_mem_32u gpuSum;
+        arr.resizeN(n);
+        gpuSum.resizeN(1);
+
+        arr.writeN(&as[0], n);
+
+        runBenchmark(arr, gpuSum, "atomicSum1", benchmarkingIters, reference_sum, n);
+        runBenchmark(arr, gpuSum, "loopSum2", benchmarkingIters, reference_sum, n);
+        runBenchmark(arr, gpuSum, "loopCoalescedSum3", benchmarkingIters, reference_sum, n);
+        runBenchmark(arr, gpuSum, "localMemSum4", benchmarkingIters, reference_sum, n);
+        runBenchmark(arr, gpuSum, "treeSum5", benchmarkingIters, reference_sum, n);
     }
 }
