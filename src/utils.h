93e5cd363ebddf304354ff5e9546e6c573c82b67
Alexander Vinogradov <alexvingra@mail.ru>
2024-09-29 22:32:16 +0300

    Add task03 solution

---
+++ src/utils.h
@@ -0,0 +1,19 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <string>
+#include <exception>
+#include <iostream>
+
+template<typename T>
+void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line)
+{
+    if (a != b) {
+        std::cerr << message << " But " << a << " != " << b << ", " << filename << ":" << line << std::endl;
+        throw std::runtime_error(message);
+    }
+}
+
+#define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
+
+#endif // UTILS_H
