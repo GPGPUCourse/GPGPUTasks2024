8e90ff089c0c799be2e8cf1bc05a2415aa6ea907
Artem Sidorenko <art.sidorenko2@gmail.com>
2024-10-13 18:37:06 +0300

    task05: fix wg size left after debug

--- src/main_merge.cpp
+++ src/main_merge.cpp
@@ -72,7 +72,7 @@ int main(int argc, char **argv) {
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             as_gpu.writeN(as.data(), n);
             t.restart();
-            gpu::WorkSize ws(16, n);
+            gpu::WorkSize ws(128, n);
             for (int block_size = 1; block_size < n; block_size *= 2) {
                 merge_global.exec(ws, as_gpu, bs_gpu, block_size);
                 as_gpu.swap(bs_gpu);
