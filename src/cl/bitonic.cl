99ccf46ff7f86de78b9cbc8a1d03778388d74475
IlyaBolkisev <bolkisev.ilya@mail.ru>
2024-10-20 19:45:55 +0500

    Added HW 6

--- src/cl/bitonic.cl
+++ src/cl/bitonic.cl
@@ -1,4 +1,26 @@
-__kernel void bitonic()
+__kernel void bitonic(__global int *data, const unsigned int stage, const unsigned int passOfStage)
 {
+    const unsigned int gidx = get_global_id(0);
 
+    unsigned int internalIdx;
+    unsigned int secondIdx;
+
+    if (passOfStage > 1) {
+        internalIdx = (gidx / (passOfStage / 2)) * passOfStage + (gidx % (passOfStage / 2));
+        secondIdx = internalIdx + (passOfStage / 2);
+    }
+    else {
+        internalIdx = gidx;
+        secondIdx = internalIdx + 1;
+    }
+
+    int first = data[internalIdx];
+    int second = data[secondIdx];
+
+    int direction = ((gidx / (stage / 2)) % 2) == 0 ? 1 : 0; // 1 - возрастающая, 0 - убывающая
+
+    if ( (direction && first > second) || (!direction && first < second) ) {
+        data[internalIdx] = second;
+        data[secondIdx] = first;
+    }
 }
--- src/main_bitonic.cpp
+++ src/main_bitonic.cpp
@@ -58,9 +58,6 @@ int main(int argc, char **argv) {
 
     const std::vector<int> cpu_sorted = computeCPU(as);
 
-    // remove me
-    return 0;
-
     gpu::gpu_mem_32i as_gpu;
     as_gpu.resizeN(n);
 
@@ -73,7 +70,19 @@ int main(int argc, char **argv) {
             as_gpu.writeN(as.data(), n);
             t.restart();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфер данных
 
-            /*TODO*/
+            for (unsigned int stage = 2; stage <= n; stage *= 2) {
+                for (unsigned int passOfStage = stage; passOfStage > 1; passOfStage /= 2) {
+                    size_t local_size = 256;
+                    size_t global_size = n / 2 + n % 2;
+
+                    bitonic.exec(
+                        gpu::WorkSize(local_size, global_size),
+                        as_gpu,
+                        stage,
+                        passOfStage
+                    );
+                }
+            }
 
             t.nextLap();
         }
