88b1beb834e0ef34fc1d123bed318d79cec46293
IskanderSabirov <sabiroviskander@yandex.ru>
2024-10-20 23:06:53 +0300

    hw06 - attempt1

--- src/cl/bitonic.cl
+++ src/cl/bitonic.cl
@@ -1,4 +1,25 @@
-__kernel void bitonic()
-{
+#ifdef __CLION_IDE__
+    #include "clion_defines.cl"
+#endif
+#line 4
 
+__kernel void bitonic(__global int *m, unsigned int chunk_log, unsigned int block_log)
+{
+    unsigned int global_i = get_global_id(0);
+
+    unsigned int chunk = global_i >> chunk_log;
+    unsigned int block = global_i >> block_log;
+
+    unsigned int local_i = global_i % (1 << block_log);
+
+    unsigned int i = (block << (block_log + 1)) + local_i;
+    unsigned int j = i + (1 << block_log);
+
+    bool activate = chunk % 2;
+    bool lower = m[i] > m[j];
+
+    if ((activate && !lower) || (!activate && lower)) {
+        unsigned int tmp = m[j];
+        m[j] = m[i]; m[i] = tmp;
+    }
 }
--- src/main_bitonic.cpp
+++ src/main_bitonic.cpp
@@ -58,9 +58,6 @@ int main(int argc, char **argv) {
 
     const std::vector<int> cpu_sorted = computeCPU(as);
 
-    // remove me
-    return 0;
-
     gpu::gpu_mem_32i as_gpu;
     as_gpu.resizeN(n);
 
@@ -73,13 +70,17 @@ int main(int argc, char **argv) {
             as_gpu.writeN(as.data(), n);
             t.restart();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфер данных
 
-            /*TODO*/
+            for (int chunk = 0; (1 << chunk) < n; chunk++) {
+                for (int delta = 0; delta <= chunk; delta++) {
+                    bitonic.exec(gpu::WorkSize(64, n / 2), as_gpu, chunk, chunk - delta);
+                }
+            }
 
             t.nextLap();
         }
 
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "GPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "GPU: " << (float(n) / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
     }
 
 
