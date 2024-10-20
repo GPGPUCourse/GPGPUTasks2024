5b278d9400194616eb01a67b56b2d5445a0ef580
Smart781 <84939218+Smart781@users.noreply.github.com>
2024-10-20 23:44:01 +0300

    Update main_bitonic.cpp

--- src/main_bitonic.cpp
+++ src/main_bitonic.cpp
@@ -70,7 +70,7 @@ int main(int argc, char **argv) {
             t.restart();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфер данных
             
             for (int i = 2; i <= n; i *= 2) {
-                for (int j = i; j > 1; j /= 2) {
+                for (int j = i; j > 0; j /= 2) {
                     gpu::WorkSize ws(128, n / 2);
                     bitonic.exec(ws, as_gpu, i, j);
                 }
