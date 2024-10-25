415991496bdfad3396a7432a910f5d5b56d91bff
Petr Ivanov <peter71302@gmail.com>
2024-10-25 18:33:16 +0500

    task07 done

--- src/cl/prefix_sum.cl
+++ src/cl/prefix_sum.cl
@@ -1,1 +1,38 @@
-// TODO
+#ifdef __CLION_IDE__
+    #include <libgpu/opencl/cl/clion_defines.cl>
+#endif
+
+
+#line 6
+
+
+__kernel void basic_prefix_sum(__global const unsigned int *as, __global unsigned int *bs, const unsigned int step, const unsigned int n)
+{
+    const unsigned int gidx = get_global_id(0);
+
+    const unsigned int left = gidx - step;
+    const unsigned int right = gidx;
+
+    if (gidx >= n)
+        return;
+
+    const unsigned int left_value = as[left];
+    const unsigned int right_value = as[right];
+
+    if (gidx >= step)
+        bs[right] = left_value + right_value;
+    else
+        bs[gidx] = right_value;
+}
+
+
+__kernel void work_efficient_prefix_sum(__global unsigned int *as, const unsigned int s, const unsigned int n, const int desc)
+{
+    const unsigned int gidx = get_global_id(0);
+
+    const unsigned int left = (desc) ? ((gidx + 1) * s * 2 - 1) : (gidx * s * 2 + s - 1);
+    const unsigned int right = left + s;
+
+    if (right < n)
+        as[right] += as[left];
+}
--- src/main_prefix_sum.cpp
+++ src/main_prefix_sum.cpp
@@ -8,7 +8,7 @@
 #include "cl/prefix_sum_cl.h"
 
 
-const int benchmarkingIters = 10;
+const int benchmarkingIters = 1;
 const int benchmarkingItersCPU = 10;
 const unsigned int max_n = (1 << 24);
 
@@ -47,61 +47,83 @@ std::vector<unsigned int> computeCPU(const std::vector<unsigned int> &as)
 
 int main(int argc, char **argv)
 {
+    gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+    gpu::Context context;
+    context.init(device.device_id_opencl);
+    context.activate();
+
+    ocl::Kernel basic_prefix_sum_kernel(prefix_sum_kernel, prefix_sum_kernel_length, "basic_prefix_sum");
+    ocl::Kernel work_efficient_prefix_sum_kernel(prefix_sum_kernel, prefix_sum_kernel_length, "work_efficient_prefix_sum");
+
 	for (unsigned int n = 4096; n <= max_n; n *= 4) {
-		std::cout << "______________________________________________" << std::endl;
-		unsigned int values_range = std::min<unsigned int>(1023, std::numeric_limits<int>::max() / n);
-		std::cout << "n=" << n << " values in range: [" << 0 << "; " << values_range << "]" << std::endl;
+	    std::cout << "______________________________________________" << std::endl;
+	    unsigned int values_range = std::min<unsigned int>(1023, std::numeric_limits<int>::max() / n);
+	    std::cout << "n=" << n << " values in range: [" << 0 << "; " << values_range << "]" << std::endl;
 
-		std::vector<unsigned int> as(n, 0);
-		FastRandom r(n);
-		for (int i = 0; i < n; ++i) {
-			as[i] = r.next(0, values_range);
-		}
+	    std::vector<unsigned int> as(n, 0);
+	    FastRandom r(n);
+	    for (int i = 0; i < n; ++i) {
+	        as[i] = r.next(0, values_range);
+	    }
 
-        const std::vector<unsigned int> cpu_reference = computeCPU(as);
+	    const std::vector<unsigned int> cpu_reference = computeCPU(as);
 
-// prefix sum
-#if 0
+	    gpu::gpu_mem_32u as_gpu;
+	    as_gpu.resizeN(n);
+	    gpu::gpu_mem_32u bs_gpu;
+	    bs_gpu.resizeN(n);
+
+	    {
+	        gpu::WorkSize workSize(256, n);
+	        std::vector<unsigned int> res(n);
+
+	        timer t;
+	        for (int i = 0; i < benchmarkingIters; ++i) {
+	            as_gpu.writeN(as.data(), n);
+	            t.restart();
+	            for (unsigned int step = 1; step < n; step *= 2) {
+	                basic_prefix_sum_kernel.exec(workSize, as_gpu, bs_gpu, step, n);
+	                std::swap(as_gpu, bs_gpu);
+	            }
+	            t.nextLap();
+	        }
+
+	        std::cout << "GPU [non-work-efficient]: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+	        std::cout << "GPU [non-work-efficient]: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
+	        as_gpu.readN(res.data(), n);
+
+	        for (int i = 0; i < n; ++i) {
+	            EXPECT_THE_SAME(res[i], cpu_reference[i], "GPU results should be equal to CPU results!");
+	        }
+	    }
+
         {
             std::vector<unsigned int> res(n);
 
             timer t;
             for (int iter = 0; iter < benchmarkingIters; ++iter) {
-                // TODO
+                as_gpu.writeN(as.data(), n);
+                unsigned int last_step;
                 t.restart();
-                // TODO
-                t.nextLap();
-            }
-
-            std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-            std::cout << "GPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
-
-            for (int i = 0; i < n; ++i) {
-                EXPECT_THE_SAME(cpu_reference[i], res[i], "GPU result should be consistent!");
-            }
-        }
-#endif
-
-// work-efficient prefix sum
-#if 0
-        {
-            std::vector<unsigned int> res(n);
-
-            timer t;
-            for (int iter = 0; iter < benchmarkingIters; ++iter) {
-                // TODO
-                t.restart();
-                // TODO
+                // наверное лучше workSize снаружи ограничивать, чем внутри кернела делать проверку и return
+                // (workitem'ов же явно больше, чем потоков, мб так драйвер лучше работу распределит)
+                for (unsigned int step = 1; step < n; step *= 2) {
+                    work_efficient_prefix_sum_kernel.exec(gpu::WorkSize(256, n / step / 2), as_gpu, step, n, 0);
+                    last_step = step;
+                }
+                for (unsigned int step = last_step / 2; step > 0; step /= 2) {
+                    work_efficient_prefix_sum_kernel.exec(gpu::WorkSize(256, n / step / 2), as_gpu, step, n, 1);
+                }
                 t.nextLap();
             }
 
             std::cout << "GPU [work-efficient]: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
             std::cout << "GPU [work-efficient]: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
+	        as_gpu.readN(res.data(), n);
 
             for (int i = 0; i < n; ++i) {
-                EXPECT_THE_SAME(cpu_reference[i], res[i], "GPU result should be consistent!");
+                EXPECT_THE_SAME(res[i], cpu_reference[i], "GPU result should be consistent!");
             }
         }
-#endif
 	}
 }
