e4f316f15a7272c8d5cccfe2810589a571acdaf3
Artem Sidorenko <art.sidorenko2@gmail.com>
2024-10-27 18:00:49 +0300

    task07: add solution for 7.1 and 7.2

--- src/cl/prefix_sum.cl
+++ src/cl/prefix_sum.cl
@@ -1,1 +1,31 @@
-// TODO
+#ifdef __CLION_IDE__
+
+#include "clion_defines.cl"
+
+#endif
+
+#line 7
+
+__kernel void pref_sum_naive(__global unsigned int *prev, __global unsigned int *next, int offset, int n) {
+    int gid = get_global_id(0);
+
+    int ind_to_add = gid + offset;
+    if (ind_to_add >= n) return;
+
+    next[ind_to_add] += prev[gid];
+}
+
+__kernel void pref_sum_efficient(__global unsigned int *ps, int offset, int n, int part) {
+    int gid = get_global_id(0);
+
+    int ind = part == 0 ?
+              (gid + 1) * (offset << 1) - 1 - offset : // first part of algo
+              (gid + 1) * (offset << 1) - 1; // second part of algo
+
+    int ind_to_add = ind + offset;
+    if (ind_to_add >= n) return;
+
+//    printf("Indices are: ind=%d, ind_to_add=%d\n", ind, ind_to_add);
+
+    ps[ind_to_add] += ps[ind];
+}
--- src/main_prefix_sum.cpp
+++ src/main_prefix_sum.cpp
@@ -47,7 +47,12 @@ std::vector<unsigned int> computeCPU(const std::vector<unsigned int> &as)
 
 int main(int argc, char **argv)
 {
-	for (unsigned int n = 4096; n <= max_n; n *= 4) {
+    gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+    gpu::Context context;
+    context.init(device.device_id_opencl);
+    context.activate();
+
+	for (unsigned int n = 4; n <= max_n; n *= 4) {
 		std::cout << "______________________________________________" << std::endl;
 		unsigned int values_range = std::min<unsigned int>(1023, std::numeric_limits<int>::max() / n);
 		std::cout << "n=" << n << " values in range: [" << 0 << "; " << values_range << "]" << std::endl;
@@ -61,17 +66,30 @@ int main(int argc, char **argv)
         const std::vector<unsigned int> cpu_reference = computeCPU(as);
 
 // prefix sum
-#if 0
+#if 1
         {
             std::vector<unsigned int> res(n);
 
+            gpu::gpu_mem_32u res_gpu, res_gpu_prev;
+            res_gpu.resizeN(n);
+            res_gpu_prev.resizeN(n);
+
+            ocl::Kernel pref_sum_naive(prefix_sum_kernel, prefix_sum_kernel_length, "pref_sum_naive");
+            pref_sum_naive.compile();
+            gpu::WorkSize ws(128, n);
+
             timer t;
             for (int iter = 0; iter < benchmarkingIters; ++iter) {
-                // TODO
+                res_gpu.writeN(as.data(), n);
                 t.restart();
-                // TODO
+                for (unsigned offset = 1; offset < n; offset <<= 1) {
+                    res_gpu.copyToN(res_gpu_prev, n);
+                    pref_sum_naive.exec(ws, res_gpu_prev, res_gpu, offset, n);
+                }
                 t.nextLap();
             }
+
+            res_gpu.readN(res.data(), n);
 
             std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
             std::cout << "GPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
@@ -83,24 +101,43 @@ int main(int argc, char **argv)
 #endif
 
 // work-efficient prefix sum
-#if 0
+#if 1
         {
             std::vector<unsigned int> res(n);
 
+            gpu::gpu_mem_32u res_gpu;
+            res_gpu.resizeN(n);
+
+            ocl::Kernel pref_sum_efficient(prefix_sum_kernel, prefix_sum_kernel_length, "pref_sum_efficient");
+            pref_sum_efficient.compile();
+
             timer t;
             for (int iter = 0; iter < benchmarkingIters; ++iter) {
-                // TODO
+                res_gpu.writeN(as.data(), n);
                 t.restart();
-                // TODO
+                for (unsigned offset = 1; offset < n; offset <<= 1) {
+//                    std::cout << "Offset: " << offset << std::endl;
+                    gpu::WorkSize ws(128, n / (offset << 1));
+                    pref_sum_efficient.exec(ws, res_gpu, offset, n, 0);
+                }
+
+                for (unsigned offset = n >> 2; offset > 0; offset >>= 1) {
+                    gpu::WorkSize ws(128, n / (offset << 1));
+                    pref_sum_efficient.exec(ws, res_gpu, offset, n, 1);
+                }
                 t.nextLap();
             }
 
             std::cout << "GPU [work-efficient]: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
             std::cout << "GPU [work-efficient]: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
 
+            res_gpu.readN(res.data(), n);
+
             for (int i = 0; i < n; ++i) {
+//                std::cout << res[i] << " ";
                 EXPECT_THE_SAME(cpu_reference[i], res[i], "GPU result should be consistent!");
             }
+//            std::cout << std::endl;
         }
 #endif
 	}
