5f2b4d2ffaa6f317148b748c0b60169ffea3047f
Axtra2 <axtra2@gmail.com>
2024-11-07 23:09:51 +0300

    task08 done

--- src/main_radix.cpp
+++ src/main_radix.cpp
@@ -7,6 +7,8 @@
 // Этот файл будет сгенерирован автоматически в момент сборки - см. convertIntoHeader в CMakeLists.txt:18
 #include "cl/radix_cl.h"
 
+#include <algorithm>
+#include <cassert>
 #include <iostream>
 #include <stdexcept>
 #include <vector>
@@ -49,6 +51,11 @@ int main(int argc, char **argv) {
     context.init(device.device_id_opencl);
     context.activate();
 
+    gpu::gpu_mem_32u as_gpu;
+    gpu::gpu_mem_32u bs_gpu;
+    gpu::gpu_mem_32u buf1_gpu;
+    gpu::gpu_mem_32u buf2_gpu;
+
     std::vector<unsigned int> as(n, 0);
     FastRandom r(n);
     for (unsigned int i = 0; i < n; ++i) {
@@ -58,21 +65,163 @@ int main(int argc, char **argv) {
 
     const std::vector<unsigned int> cpu_reference = computeCPU(as);
 
-    // remove me
-    return 0;
+    const unsigned int work_group_size = 128;
+    const unsigned int nbits = 4;
+    const unsigned int nwork_groups = (n + work_group_size - 1) / work_group_size;
+    const unsigned int buf_size = nwork_groups * (1 << nbits);
 
     {
+        as_gpu.resizeN(n);
+        bs_gpu.resizeN(n);
+        buf1_gpu.resizeN(buf_size);
+        buf2_gpu.resizeN(buf_size);
+
+        ocl::Kernel radix_write_zeros(
+            radix_kernel,
+            radix_kernel_length,
+            "write_zeros"
+        );
+        radix_write_zeros.compile();
+
+        ocl::Kernel radix_count(
+            radix_kernel,
+            radix_kernel_length,
+            "count"
+        );
+        radix_count.compile();
+
+        ocl::Kernel radix_transpose(
+            radix_kernel,
+            radix_kernel_length,
+            "transpose"
+        );
+        radix_transpose.compile();
+
+        ocl::Kernel radix_prefix_sum_pass1(
+            radix_kernel,
+            radix_kernel_length,
+            "prefix_sum_pass1"
+        );
+        radix_prefix_sum_pass1.compile();
+
+        ocl::Kernel radix_prefix_sum_pass2(
+            radix_kernel,
+            radix_kernel_length,
+            "prefix_sum_pass2"
+        );
+        radix_prefix_sum_pass2.compile();
+
+        ocl::Kernel radix_sort(
+            radix_kernel,
+            radix_kernel_length,
+            "sort"
+        );
+        radix_sort.compile();
+
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            // Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфер данных
+            as_gpu.writeN(as.data(), n);
 
-            // TODO
+            // Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфер данных
+            t.restart();
+
+            for (
+                unsigned int bit_shift = 0;
+                bit_shift < 8 * sizeof(unsigned int);
+                bit_shift += nbits
+            ) {
+                // write zeros
+                {
+                    const unsigned int work_size = nwork_groups * (1 << nbits);
+                    radix_write_zeros.exec(
+                        gpu::WorkSize(work_group_size, work_size),
+                        buf1_gpu,
+                        buf_size
+                    );
+                }
+
+                // count
+                {
+                    const unsigned int work_size = nwork_groups * work_group_size;
+                    radix_count.exec(
+                        gpu::WorkSize(work_group_size, work_size),
+                        as_gpu,
+                        n,
+                        buf1_gpu,
+                        bit_shift
+                    );
+                }
+
+                // transpose
+                {
+                    const unsigned int M = nwork_groups;
+                    const unsigned int K = 1 << nbits;
+                    const unsigned int tile_size = std::min({M, K, 16u});
+                    assert((tile_size & (tile_size - 1)) == 0 && "tile_size should be a power of 2");
+                    radix_transpose.exec(
+                        gpu::WorkSize(
+                            tile_size,
+                            tile_size,
+                            tile_size * ((K + tile_size - 1) / tile_size),
+                            tile_size * ((M + tile_size - 1) / tile_size)
+                        ),
+                        buf1_gpu,
+                        buf2_gpu,
+                        M,
+                        K
+                    );
+                }
+
+                // prefix sum
+                {
+                    for (int block_size = 2; block_size <= buf_size; block_size *= 2) {
+                        unsigned int work_size = buf_size / block_size;
+                        work_size = work_group_size * ((work_size + work_group_size - 1) / work_group_size);
+                        radix_prefix_sum_pass1.exec(
+                            gpu::WorkSize(work_group_size, work_size),
+                            buf2_gpu,
+                            buf_size,
+                            block_size
+                        );
+                    }
+
+                    for (int block_size = buf_size / 4; block_size > 0; block_size /= 2) {
+                        unsigned int work_size = buf_size / (block_size * 2) - 1;
+                        work_size = work_group_size * ((work_size + work_group_size - 1) / work_group_size);
+                        radix_prefix_sum_pass2.exec(
+                            gpu::WorkSize(work_group_size, work_size),
+                            buf2_gpu,
+                            buf_size,
+                            block_size
+                        );
+                    }
+                }
+
+                // sort
+                {
+                    const unsigned int work_size = nwork_groups * work_group_size;
+                    radix_sort.exec(
+                        gpu::WorkSize(work_group_size, work_size),
+                        as_gpu,
+                        bs_gpu,
+                        n,
+                        buf2_gpu,
+                        bit_shift
+                    );
+                }
+
+                as_gpu.swap(bs_gpu);
+
+                t.nextLap();
+            }
         }
         t.stop();
 
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "GPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
     }
+
+    as_gpu.readN(as.data(), n);
 
     // Проверяем корректность результатов
     for (int i = 0; i < n; ++i) {
