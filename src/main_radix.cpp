6ab89b802a0ad0de96de2be030a0e179675e9f6f
Artem Sidorenko <art.sidorenko2@gmail.com>
2024-11-10 22:01:25 +0300

    task08: solution added

--- src/main_radix.cpp
+++ src/main_radix.cpp
@@ -25,8 +25,7 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
-std::vector<unsigned int> computeCPU(const std::vector<unsigned int> &as)
-{
+std::vector<unsigned int> computeCPU(const std::vector<unsigned int> &as) {
     std::vector<unsigned int> cpu_sorted;
 
     timer t;
@@ -58,21 +57,108 @@ int main(int argc, char **argv) {
 
     const std::vector<unsigned int> cpu_reference = computeCPU(as);
 
-    // remove me
-    return 0;
-
+    gpu::gpu_mem_32u as_gpu;
+    as_gpu.resizeN(n);
+    as_gpu.writeN(as.data(), n);
     {
+        ocl::Kernel reset_to_zeros(radix_kernel, radix_kernel_length, "reset_to_zeros");
+        reset_to_zeros.compile();
+        ocl::Kernel count(radix_kernel, radix_kernel_length, "count");
+        count.compile();
+        ocl::Kernel pref_sum(radix_kernel, radix_kernel_length, "pref_sum");
+        pref_sum.compile();
+        ocl::Kernel matrix_transpose(radix_kernel, radix_kernel_length, "matrix_transpose");
+        matrix_transpose.compile();
+        ocl::Kernel radix_sort(radix_kernel, radix_kernel_length, "radix_sort");
+        radix_sort.compile();
+
+        const int wg_size = 128;
+        const int nbits = 4;
+        const int uint_size = 32;
+
+        gpu::gpu_mem_32u cs_gpu;
+        cs_gpu.resizeN((1 << nbits) * n / wg_size);
+        gpu::gpu_mem_32u cs_t_gpu;
+        cs_t_gpu.resizeN((1 << nbits) * n / wg_size);
+        gpu::gpu_mem_32u res_gpu;
+        res_gpu.resizeN(n);
+
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            // Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфер данных
+            t.restart();
+            for (int bit_shift = 0; bit_shift < uint_size; bit_shift += nbits) {
+                reset_to_zeros.exec(gpu::WorkSize(128, (1 << nbits) * n / wg_size), cs_gpu);
 
-            // TODO
+                count.exec(gpu::WorkSize(wg_size, n), as_gpu, cs_gpu, bit_shift, nbits);
+
+                matrix_transpose.exec(
+                        gpu::WorkSize(16, 16, (1 << nbits), n / wg_size),
+                        cs_gpu, cs_t_gpu,
+                        (1 << nbits),
+                        n / wg_size
+                );
+
+//                {
+//                    std::vector<unsigned> cs(n / wg_size * (1 << nbits));
+//                    cs_gpu.readN(cs.data(), cs.size());
+//                    std::vector<unsigned> cs_t(cs.size());
+//                    cs_t_gpu.readN(cs_t.data(), cs_t.size());
+//
+//                    for (int i = 0; i < n / wg_size; i++) {
+//                        for (int j = 0; j < (1 << nbits); j++) {
+//                            std::cout << cs[i * (1 << nbits) + j] << " ";
+//                        }
+//                        std::cout << std::endl;
+//                    }
+//                    std::cout << "=======================" << std::endl;
+//                    for (int i = 0; i < (1 << nbits); i++) {
+//                        for (int j = 0; j < (n / wg_size); j++) {
+//                            std::cout << cs_t[i * (n / wg_size) + j] << " ";
+//                        }
+//                        std::cout << std::endl;
+//                    }
+//                }
+
+
+                for (unsigned offset = 1; offset < (1 << nbits) * n / wg_size; offset <<= 1) {
+                    gpu::WorkSize ws(wg_size, (1 << nbits) * n / wg_size / (offset << 1));
+                    pref_sum.exec(ws, cs_t_gpu, offset, (1 << nbits) * n / wg_size, 0);
+                }
+                for (unsigned offset = (1 << nbits) * n / wg_size >> 2; offset > 0; offset >>= 1) {
+                    gpu::WorkSize ws(wg_size, (1 << nbits) * n / wg_size / (offset << 1));
+                    pref_sum.exec(ws, cs_t_gpu, offset, (1 << nbits) * n / wg_size, 1);
+                }
+
+//                {
+//                    std::vector<unsigned> cs_t((1 << nbits) * n / wg_size);
+//                    cs_t_gpu.readN(cs_t.data(), cs_t.size());
+//
+//                    for (int i = 0; i < (1 << nbits); i++) {
+//                        for (int j = 0; j < (n / wg_size); j++) {
+//                            std::cout << cs_t[i * (n / wg_size) + j] << " ";
+//                        }
+//                        std::cout << std::endl;
+//                    }
+//                }
+
+                radix_sort.exec(
+                        gpu::WorkSize(128, n),
+                        as_gpu, res_gpu,
+                        cs_t_gpu,
+                        bit_shift, nbits
+                );
+
+                std::swap(as_gpu, res_gpu);
+            }
+            t.nextLap();
         }
         t.stop();
 
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "GPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
     }
+
+    as_gpu.readN(as.data(), n);
 
     // Проверяем корректность результатов
     for (int i = 0; i < n; ++i) {
