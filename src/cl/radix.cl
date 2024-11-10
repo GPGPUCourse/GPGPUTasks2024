d35ecc121ca04ce367fc13e1433c304455d3571d
Denil Sharipov <camm2016@yandex.ru>
2024-11-10 23:36:04 +0300

    + task08

--- src/cl/radix.cl
+++ src/cl/radix.cl
@@ -1,1 +1,84 @@
-// TODO
+#define BITS 4
+#define WG_SIZE 128
+#define TILE_SIZE 16
+
+int get_bits(unsigned int a, unsigned int shift) {
+    return (a >> shift) & ((1 << BITS) - 1);
+}
+
+__kernel void assign_zeros(__global unsigned int* a)
+{
+    int gid = get_global_id(0);
+    a[gid] = 0;
+}
+
+__kernel void count_workgroup(
+    __global const unsigned int* a, 
+    __global unsigned int* cnt,
+    unsigned int shift)
+{
+    int gid = get_global_id(0);
+    int wid = get_group_id(0);
+    atomic_inc(&cnt[wid * (1 << BITS) + get_bits(a[gid], shift)]);
+}
+
+__kernel void reduce(__global const unsigned int* a, __global unsigned int* b) {
+    int i = get_global_id(0);
+
+    b[i] = a[2 * i] + a[2 * i + 1];
+}
+
+__kernel void down_sweep(__global const unsigned int* a, __global unsigned int* b) {
+    int i = get_global_id(0);
+
+    if (i == 0) {
+        return;
+    }
+
+    if (i % 2) {
+        b[i] = a[i / 2];
+    } else {
+        b[i] += a[i / 2 - 1];
+    }
+}
+
+__kernel void matrix_transpose(__global const float* a, __global float* at, unsigned int m, unsigned int k)
+{
+    int i = get_global_id(0);
+    int j = get_global_id(1);
+
+    __local float tile[TILE_SIZE][TILE_SIZE + 1];
+
+    int local_i = get_local_id(0);
+    int local_j = get_local_id(1);
+
+    tile[local_j][local_i] = a[j * k + i];
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    int i_res = get_group_id(0) * TILE_SIZE + local_j;
+    int j_res = get_group_id(1) * TILE_SIZE + local_i;
+    at[i_res * m + j_res] = tile[local_i][local_j];
+}
+
+__kernel void radix_sort(
+    __global const unsigned int* a,
+    __global unsigned int* b, 
+    __global unsigned int* cnt,
+    unsigned int wg_cnt,
+    unsigned int shift)
+{
+    int lid = get_local_id(0);
+    int wid = get_group_id(0);
+    int gid = get_global_id(0);
+
+    int num = get_bits(a[gid], shift);
+    int cnt_idx = num * wg_cnt + wid - 1;
+    int idx = cnt_idx < 0 ? 0 : cnt[cnt_idx];
+
+    for (int i = wid * WG_SIZE; i < wid * WG_SIZE + lid; i++) {
+        idx += get_bits(a[i], shift) == num;
+    }
+
+    b[idx] = a[gid];
+}
--- src/main_radix.cpp
+++ src/main_radix.cpp
@@ -13,7 +13,10 @@
 
 const int benchmarkingIters = 10;
 const int benchmarkingItersCPU = 1;
-const unsigned int n = 32 * 1024 * 1024;
+const unsigned int n = 32 * 1024; //* 1024;
+const unsigned int wg_size = 128;
+const unsigned int wg_tp_size = 16;
+const unsigned int bits = 4;
 
 template<typename T>
 void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line) {
@@ -42,6 +45,27 @@ std::vector<unsigned int> computeCPU(const std::vector<unsigned int> &as)
     return cpu_sorted;
 }
 
+void prefix_sum(ocl::Kernel reduce, ocl::Kernel down_sweep, gpu::gpu_mem_32u& res_gpu, unsigned int n) {
+    std::vector<gpu::gpu_mem_32u> as_gpus;
+    for(int d = 1; d <= n; d <<= 1) {
+        gpu::gpu_mem_32u as_gpu;
+        as_gpu.resizeN(n / d);
+        as_gpus.push_back(std::move(as_gpu));
+    }
+
+    as_gpus[0] = res_gpu;
+    for (int logd = 1; logd < as_gpus.size(); logd++) {
+        const unsigned int curn = n / (1 << logd);
+        reduce.exec(gpu::WorkSize(std::min(curn, wg_size), curn), as_gpus[logd - 1], as_gpus[logd]);
+    }
+    for (int logd = as_gpus.size() - 2; logd >= 0; logd--) {
+        const unsigned int curn = n / (1 << logd);
+        down_sweep.exec(gpu::WorkSize(std::min(curn, wg_size), curn), as_gpus[logd + 1], as_gpus[logd]);
+    }
+
+    res_gpu = as_gpus[0];
+}
+
 int main(int argc, char **argv) {
     gpu::Device device = gpu::chooseGPUDevice(argc, argv);
 
@@ -58,17 +82,57 @@ int main(int argc, char **argv) {
 
     const std::vector<unsigned int> cpu_reference = computeCPU(as);
 
-    // remove me
-    return 0;
+    {   
+        ocl::Kernel assign_zeros(radix_kernel, radix_kernel_length, "assign_zeros");
+        assign_zeros.compile();
 
-    {
+        ocl::Kernel count_workgroup(radix_kernel, radix_kernel_length, "count_workgroup");
+        count_workgroup.compile();
+
+        ocl::Kernel matrix_transpose(radix_kernel, radix_kernel_length, "matrix_transpose");
+        matrix_transpose.compile();
+
+        ocl::Kernel reduce(radix_kernel, radix_kernel_length, "reduce");
+        reduce.compile();
+
+        ocl::Kernel down_sweep(radix_kernel, radix_kernel_length, "down_sweep");
+        down_sweep.compile();
+
+        ocl::Kernel radix_sort(radix_kernel, radix_kernel_length, "radix_sort");
+        radix_sort.compile();
+
+        const unsigned int wg_cnt = n / wg_size;
+        const unsigned int cnt_size = wg_cnt * (1 << bits);
+
+        gpu::gpu_mem_32u as_gpu, bs_gpu, cnt_gpu, cnt_gpu_tp;
+        as_gpu.resizeN(n);
+        bs_gpu.resizeN(n);
+        cnt_gpu.resizeN(cnt_size);
+        cnt_gpu_tp.resizeN(cnt_size);
+
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            // Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфер данных
+            as_gpu.writeN(as.data(), as.size());
+            t.restart();
 
-            // TODO
+            for (int shift = 0; shift < 32; shift += bits) {
+                assign_zeros.exec(gpu::WorkSize(wg_size, cnt_size), cnt_gpu);
+
+                count_workgroup.exec(gpu::WorkSize(wg_size, n), as_gpu, cnt_gpu, shift);
+
+                matrix_transpose.exec(gpu::WorkSize(wg_tp_size, wg_tp_size, 1 << bits, wg_cnt), cnt_gpu, cnt_gpu_tp, wg_cnt, 1 << bits);
+
+                prefix_sum(reduce, down_sweep, cnt_gpu_tp, cnt_size);
+
+                radix_sort.exec(gpu::WorkSize(wg_size, n), as_gpu, bs_gpu, cnt_gpu_tp, wg_cnt, shift);
+
+                std::swap(as_gpu, bs_gpu);
+            }
+            t.nextLap();
         }
         t.stop();
+
+        as_gpu.readN(as.data(), n);
 
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "GPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
