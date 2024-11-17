993b6477996c3ba5f44bbde5a881bea6d7aca60a
Valery Matskevich <generalretcher@gmail.com>
2024-11-17 14:44:39 +0300

    task09: monster is alive
    Signed-off-by: Valery Matskevich <generalretcher@gmail.com>

--- src/sdf_main.glsl
+++ src/sdf_main.glsl
@@ -11,6 +11,14 @@ float sdPlane(vec3 p)
     return p.y;
 }
 
+// Capsule / Line - exact
+float sdCapsule( vec3 p, vec3 a, vec3 b, float r )
+{
+  vec3 pa = p - a, ba = b - a;
+  float h = clamp( dot(pa,ba)/dot(ba,ba), 0.0, 1.0 );
+  return length( pa - ba*h ) - r;
+}
+
 // косинус который пропускает некоторые периоды, удобно чтобы махать ручкой не все время
 float lazycos(float angle)
 {
@@ -24,23 +32,68 @@ float lazycos(float angle)
     return 1.0;
 }
 
+// quadratic polynomial
+float smin( float a, float b, float k )
+{
+    k *= 4.0;
+    float h = max( k-abs(a-b), 0.0 )/k;
+    return min(a,b) - h*h*k*(1.0/4.0);
+}
+
 // возможно, для конструирования тела пригодятся какие-то примитивы из набора https://iquilezles.org/articles/distfunctions/
 // способ сделать гладкий переход между примитивами: https://iquilezles.org/articles/smin/
 vec4 sdBody(vec3 p)
 {
+    float K_SMIN = 0.025;
+    float K_SMIN_HANDS = 0.01;
+    float HAND_MOVEMENT_SPEEDUP = 7.0;
+    float LEG_K = 0.03;
+
     float d = 1e10;
 
-    // TODO
-    d = sdSphere((p - vec3(0.0, 0.35, -0.7)), 0.35);
-    
+    d = sdSphere((p - vec3(0.0, 0.35, -0.7)), 0.3);
+    d = smin(d, sdSphere((p - vec3(0.0, 0.6, -0.7)), 0.20), K_SMIN);
+
+#define LEG_P 0.125, 0.01, -0.7
+#define LEG_A 0.0, 0.7, 0.0
+#define LEG_B 0.03, -0.05, 0.01
+    d = smin(d, sdCapsule((p - vec3(LEG_P)), vec3(LEG_A), vec3(-LEG_B), LEG_K), K_SMIN);
+    d = smin(d, sdCapsule((p - vec3(-LEG_P)), vec3(LEG_A), vec3(LEG_B), LEG_K), K_SMIN);
+#undef LEG_B
+#undef LEG_A
+#undef LEG_P
+
+#define HAND_P 0.27, 0.36, -0.6
+#define HAND_A 0.09, 0.1, 0.0
+    d = smin(d, sdCapsule((p - vec3(HAND_P)), vec3(-HAND_A), vec3(0.1, -0.05, 0.15), 0.04), K_SMIN_HANDS);
+    d = smin(d, sdCapsule((p - vec3(-HAND_P)), vec3(HAND_A), vec3(-0.1, -0.05 + 0.07 * sin(HAND_MOVEMENT_SPEEDUP * iTime), 0.05 + 0.07 * lazycos(HAND_MOVEMENT_SPEEDUP * iTime)), 0.04), K_SMIN_HANDS);
+#undef HAND_A
+#undef HAND_P
+
     // return distance and color
     return vec4(d, vec3(0.0, 1.0, 0.0));
 }
 
 vec4 sdEye(vec3 p)
 {
+    float radiusEye = 0.15;
+    float radiusRim = 0.08;
+    float radiusPupil = 0.05;
 
-    vec4 res = vec4(1e10, 0.0, 0.0, 0.0);
+    float dEye = sdSphere(p + vec3(0.0, -0.4, 0.4), radiusEye);
+    float dRim = sdSphere(p + vec3(0.0, -0.4, 0.3), radiusRim);
+    float dPupil = sdSphere(p + vec3(0.0, -0.4, 0.25), radiusPupil);
+
+    vec4 res = vec4(dEye, vec3(1.0, 1.0, 1.0));
+    vec4 rim = vec4(dRim, vec3(0.1, 0.4, 1.0));
+    vec4 pupil = vec4(dPupil, vec3(0.0, 0.0, 0.0));
+
+    if (rim.x < res.x) {
+        res = rim;
+    }
+    if (pupil.x < res.x) {
+        res = pupil;
+    }
     
     return res;
 }
