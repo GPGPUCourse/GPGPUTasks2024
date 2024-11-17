86a4abe8728f837a559304cb3c81ce396571524c
mozhaa <mozhay2005@gmail.com>
2024-11-17 12:26:51 +0300

    done

--- src/sdf_main.glsl
+++ src/sdf_main.glsl
@@ -1,4 +1,3 @@
-
 // sphere with center in (0, 0, 0)
 float sdSphere(vec3 p, float r)
 {
@@ -11,27 +10,52 @@ float sdPlane(vec3 p)
     return p.y;
 }
 
-// косинус который пропускает некоторые периоды, удобно чтобы махать ручкой не все время
-float lazycos(float angle)
+float sdCapsule( vec3 p, vec3 a, vec3 b, float r )
 {
+  vec3 pa = p - a, ba = b - a;
+  float h = clamp( dot(pa,ba)/dot(ba,ba), 0.0, 1.0 );
+  return length( pa - ba*h ) - r;
+}
+
+float opSmoothUnion( float d1, float d2, float k )
+{
+    float h = clamp( 0.5 + 0.5*(d2-d1)/k, 0.0, 1.0 );
+    return mix( d2, d1, h ) - k*h*(1.0-h);
+}
+
+// тело монстрика
+float sdIDK(vec3 p, float r1, float r2, vec3 dir, float k) {
+    return opSmoothUnion(sdSphere(p, r1), sdSphere(p + dir, r2), k);
+}
+
+
+float lazyAngle(float angle) {
     int nsleep = 10;
     
     int iperiod = int(angle / 6.28318530718) % nsleep;
     if (iperiod < 3) {
-        return cos(angle);
+        return angle;
     }
     
-    return 1.0;
+    return 0.0;
+}
+
+// косинус который пропускает некоторые периоды, удобно чтобы махать ручкой не все время
+float lazycos(float angle)
+{
+    return cos(lazyAngle(angle));
 }
 
 // возможно, для конструирования тела пригодятся какие-то примитивы из набора https://iquilezles.org/articles/distfunctions/
 // способ сделать гладкий переход между примитивами: https://iquilezles.org/articles/smin/
 vec4 sdBody(vec3 p)
 {
-    float d = 1e10;
+    vec3 center = vec3(0.0, 0.35, -0.7);
+    float r1 = 0.35;
+    float r2 = 0.25;
+    float h = 0.28;
 
-    // TODO
-    d = sdSphere((p - vec3(0.0, 0.35, -0.7)), 0.35);
+    float d = sdIDK(p - center, r1, r2, vec3(0.0, -h, 0.0), 0.22);
     
     // return distance and color
     return vec4(d, vec3(0.0, 1.0, 0.0));
@@ -39,9 +63,58 @@ vec4 sdBody(vec3 p)
 
 vec4 sdEye(vec3 p)
 {
-
-    vec4 res = vec4(1e10, 0.0, 0.0, 0.0);
+    vec3 center = vec3(0.0, 0.6, -0.52);
+    float r = 0.2;
+    float d = sdSphere((p - center), r);
+    vec3 eyeDirection = vec3(0.0, 0.2, 1.0);
+    float angle = max(0.0, dot(normalize(p - center), normalize(eyeDirection)));
+    vec3 color = vec3(1.0, 1.0, 1.0);
+    if (angle > 0.95) {
+        color = vec3(0.0, 0.0, 0.0);
+    } else if (angle > 0.85) {
+        color = vec3(0.0, 1.0, 1.0);
+    }
     
+    return vec4(d, color);
+}
+
+// повернуть вектор p вокруг axis с центром в center на угол angle
+vec3 rotate(vec3 p, vec3 axis, vec3 center, float angle) {
+    p -= center;
+    axis = normalize(axis);
+    float ax = axis.x * angle;
+    float ay = axis.y * angle;
+    float az = axis.z * angle;
+    mat3 rx = mat3(1.0, 0.0, 0.0, 0.0, cos(ax), -sin(ax), 0.0, sin(ax), cos(ax));
+    mat3 ry = mat3(cos(ay), 0.0, sin(ay), 0.0, 1.0, 0.0, -sin(ay), 0.0, cos(ay));
+    mat3 rz = mat3(cos(az), -sin(az), 0.0, sin(az), cos(az), 0.0, 0.0, 0.0, 1.0);
+    return rx * ry * rz * p + center;
+}
+
+vec4 process(vec4 res, vec4 d) {
+    if (d.x < res.x) {
+        return d;
+    } else {
+        return res;
+    }
+}
+
+vec4 sdLimbs(vec3 p) {
+    vec3 c1 = vec3(-0.1, 0.05, -0.7);
+    vec3 c2 = vec3(0.1, 0.05, -0.7);
+    vec3 c3 = vec3(-0.3, 0.4, -0.7);
+    vec3 c4 = vec3(0.3, 0.4, -0.7);
+    
+    vec3 legDir = vec3(0.0, -0.08, 0.0);
+    vec3 armDir1 = vec3(-0.15, -0.08, 0.0);
+    armDir1 = rotate(armDir1, vec3(-1.0, 0.0, 0.0), vec3(0.0, 0.0, 0.0), lazyAngle(iTime * 8.0));
+    vec3 armDir2 = vec3(0.15, -0.08, 0.0);
+    vec3 color = vec3(0.0, 1.0, 0.0);
+    
+    vec4 res = vec4(sdCapsule(p, c1, c1 + legDir, 0.05), color);
+    res = process(res, vec4(sdCapsule(p, c2, c2 + legDir, 0.05), color));
+    res = process(res, vec4(sdCapsule(p, c3, c3 + armDir1, 0.05), color));
+    res = process(res, vec4(sdCapsule(p, c4, c4 + armDir2, 0.05), color));
     return res;
 }
 
@@ -51,12 +124,14 @@ vec4 sdMonster(vec3 p)
     // модифицировать p, чтобы двигать объект как целое
     p -= vec3(0.0, 0.08, 0.0);
     
-    vec4 res = sdBody(p);
+    // поворот монстрика вокруг своей оси
+    vec3 axis = vec3(0.0, 1.0, 0.0); 
+    vec3 center = vec3(0.0, 0.0, -0.7);
+    // p = rotate(p, axis, center, iTime);
     
-    vec4 eye = sdEye(p);
-    if (eye.x < res.x) {
-        res = eye;
-    }
+    vec4 res = sdBody(p);
+    res = process(res, sdEye(p));
+    res = process(res, sdLimbs(p));
     
     return res;
 }
@@ -182,3 +257,4 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     // Output to screen
     fragColor = vec4(col, 1.0);
 }
+
